0dbbe4990eaa6262da46799af0cefb21f39bec98
LucasA <37451939+lucasaraujonrt@users.noreply.github.com>
2020-05-11 18:51:11 -0300

    Done

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -3,6 +3,7 @@ import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import api from "../../services/api";
 import "./styles.css";
+import Logon from "../Logon/index";
 
 import logoImg from "../../assets/logo.svg";
 
@@ -21,7 +22,7 @@ export default function Register() {
       email,
       whatsapp,
       city,
-      uf
+      uf,
     };
     try {
       const response = await api.post("ongs", data);
@@ -50,30 +51,30 @@ export default function Register() {
           <input
             placeholder="Nome da ONG"
             value={name}
-            onChange={e => setName(e.target.value)}
+            onChange={(e) => setName(e.target.value)}
           />
           <input
             type="email"
             placeholder="E-mail"
             value={email}
-            onChange={e => setEmail(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <input
             placeholder="WhatsaApp"
             value={whatsapp}
-            onChange={e => setWhatsapp(e.target.value)}
+            onChange={(e) => setWhatsapp(e.target.value)}
           />
           <div className="input-group">
             <input
               placeholder="Cidade"
               value={city}
-              onChange={e => setCity(e.target.value)}
+              onChange={(e) => setCity(e.target.value)}
             />
             <input
               placeholder="UF"
               style={{ width: 80 }}
               value={uf}
-              onChange={e => setUf(e.target.value)}
+              onChange={(e) => setUf(e.target.value)}
             />
           </div>
           <button className="button" type="submit">
